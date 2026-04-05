dab34cd1faa795cbb50e92aab21392daba0eaac4
naufalfzml <naufalfzml07@gmail.com>
2026-04-06 02:22:07 +0700

    feat: refund instruction

---
+++ programs/solana-crowdfunding/src/instructions/refund.rs
@@ -0,0 +1,54 @@
+use anchor_lang::prelude::*;
+use crate::state::{Campaign, Contribution};
+
+#[derive(Accounts)]
+pub struct Refund<'info> {
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+
+    #[account(mut)]
+    pub donor: Signer<'info>,
+
+    #[account(
+        mut, 
+        seeds= [b"vault", campaign.key().as_ref()],
+        bump,
+    )]
+    pub vault: SystemAccount<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"contribution", campaign.key().as_ref(), donor.key().as_ref()],
+        bump,
+    )]
+    pub contribution: Account<'info, Contribution>,
+
+    pub system_program:Program<'info, System>,
+}
+
+pub fn handler(ctx: Context<Refund>) -> Result<()> {
+    let campaign = &mut ctx.accounts.campaign;
+    let contribution = &mut ctx.accounts.contribution;
+    let amount = contribution.amount;
+
+    let clock = Clock::get()?;
+    let current_time = clock.unix_timestamp;
+
+    require!(campaign.raised < campaign.goal);
+    require!(current_time > campaign.deadline);
+
+    system_program::transfer(
+        CpiContext::new_with_signer(
+            ctx.accounts.system_program.to_account_info(),
+            system_program::Transfer {
+                from: ctx.accounts.vault.to_account_info(),
+                to: ctx.accounts.donor.to_account_info(),
+            },
+            &[&[b"vault", ctx.accounts.campaign.key().as_ref(), &[ctx.bumps.vault]]],
+        ),
+        amount,
+    )?;
+    contribution.amount = 0;
+    campaign.raised -= amount;
+    Ok(())
+}
